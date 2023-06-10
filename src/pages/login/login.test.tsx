1c4dfeb11388cd798d8ea36fd5f26aebdfff7f9d
th3mon <p.szelenberger@gmail.com>
2023-06-10 23:53:46 +0200

    Prepare Login Page HTML

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -1,5 +1,5 @@
 import { Login } from '@/pages/login/login';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 describe('Login Page', () => {
   it('should exist', () => {
@@ -8,5 +8,47 @@ describe('Login Page', () => {
 
   it('should display the login page', () => {
     render(<Login />);
+  });
+
+  it('should display the login page with header', () => {
+    render(<Login />);
+
+    const header: HTMLElement = screen.getByText(/Hi there!/i);
+
+    expect(header).toBeInTheDocument();
+  });
+
+  it('should the login page node has login-page Class Name', () => {
+    render(<Login />);
+
+    const loginPage: HTMLElement = screen.getByTestId('login-page');
+
+    expect(loginPage).toHaveClass('login-page');
+  });
+
+  it('should display the login page with login form', () => {
+    render(<Login />);
+
+    const form: HTMLElement = screen.getByTestId('login-form');
+
+    expect(form).toBeInTheDocument();
+  });
+
+  it('should display the login page with login form which has user name and password fields', () => {
+    render(<Login />);
+
+    const loginField: HTMLElement = screen.getByLabelText(/login/i);
+    const passwordField: HTMLElement = screen.getByLabelText(/password/i);
+
+    expect(loginField).toBeInTheDocument();
+    expect(passwordField).toBeInTheDocument();
+  });
+
+  it('should display the login page with login button', () => {
+    render(<Login />);
+
+    const loginButton: HTMLElement = screen.getByText(/Sign In/i);
+
+    expect(loginButton).toBeInTheDocument();
   });
 });
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
 export const Login: React.FunctionComponent = function () {
-  return <div>Login</div>;
+  return (
+    <div className="login-page" data-testid="login-page">
+      <header>
+        <h1>Hi there!</h1>
+      </header>
+
+      <form data-testid="login-form">
+        <div data-testid="login-field">
+          <label htmlFor="login">login</label>
+          <input type="text" name="login" id="login" />
+        </div>
+
+        <div data-testid="password-field">
+          <label htmlFor="password">password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+
+        <button type="submit" data-testid="login-button">
+          Sign in
+        </button>
+      </form>
+    </div>
+  );
 };
